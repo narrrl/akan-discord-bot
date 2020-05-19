8103a8befa5786b9b6144a19250696f723fa00c5
Nils Pukropp <nils.pukropp@outlook.de>
2020-05-19 21:51:08 +0200

    created

---
+++ src/main/java/de/nirusu99/akan/Main.java
@@ -0,0 +1,13 @@
+package de.nirusu99.akan;
+
+import net.dv8tion.jda.api.AccountType;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.sharding.DefaultShardManagerBuilder;
+
+public class Main {
+    public static void main(String[] args) throws Exception
+    {
+        JDABuilder builder = new JDABuilder(AccountType.BOT);
+        builder.setToken()
+    }
+}
