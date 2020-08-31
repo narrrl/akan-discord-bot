837e0dd568d70f96cad6e603df1bb89054333db3
Nils Pukropp <nils.pukropp@outlook.de>
2020-09-01 00:20:39 +0200

    added neko command

---
+++ src/main/java/de/nirusu99/akan/commands/fun/Neko.java
@@ -0,0 +1,90 @@
+package de.nirusu99.akan.commands.fun;
+
+import de.nirusu99.akan.commands.CommandContext;
+import de.nirusu99.akan.commands.ICommand;
+import de.nirusu99.akan.images.Host;
+import de.nirusu99.akan.images.Image;
+import net.dv8tion.jda.api.EmbedBuilder;
+
+import org.kohsuke.MetaInfServices;
+
+import javax.annotation.Nonnull;
+
+import java.awt.Color;
+import java.io.IOException;
+import java.util.Collection;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * This {@link ICommand} repeats the users input converted to emotes.
+ * The emotes get converted in {@link EmoteConverter}
+ *
+ * @author Nils Pukropp
+ * @since 1.0
+ */
+@MetaInfServices(ICommand.class)
+public final class Neko implements ICommand {
+    private static final Pattern PATTERN = Pattern.compile("neko( \\d+)?");
+
+    @Override
+    public void run(@Nonnull CommandContext ctx) {
+        List<String> args = ctx.getArgs();
+        Collection<Image> images = null;
+        int amount;
+        if (args.isEmpty()) {
+            amount = 1;
+        } else {
+            try {
+                amount = Integer.parseInt(args.get(0));
+            } catch (NumberFormatException e) {
+                e.printStackTrace();
+                ctx.getChannel().sendMessage("Wups something went wrong, contact admin pls :(").queue();
+                return;
+            }
+        }
+        try {
+            images = Host.getNekos("neko", amount);
+        } catch (IOException e) {
+            e.printStackTrace();
+            ctx.getChannel().sendMessage("Wups something went wrong, contact admin pls :(").queue();
+            return;
+        }
+
+        amount = amount > 5 ? 5 : amount;
+
+        EmbedBuilder emb;
+
+        for (Image image : images) {
+            emb = new EmbedBuilder();
+            emb.setColor(Color.PINK).setImage(image.getUrl()).setTitle("Nya~", image.getUrl());
+            ctx.getChannel().sendMessage(emb.build()).queue();;
+        }
+    }
+
+    @Override
+    public String getName() {
+        return "neko";
+    }
+
+    @Override
+    public String syntax() {
+        return "<prefix>neko <amount>";
+    }
+
+    @Override
+    public String gifHelpUrl() {
+        return null;
+    }
+
+    @Override
+    public Matcher matches(String input) {
+        return PATTERN.matcher(input);
+    }
+
+    @Override
+    public String toString() {
+        return "some neko pics :D";
+    }
+}
--- src/main/java/de/nirusu99/akan/images/Host.java
+++ src/main/java/de/nirusu99/akan/images/Host.java
@@ -1,49 +1,53 @@
 package de.nirusu99.akan.images;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.URL;
 import java.util.Set;
 import java.util.HashSet;
 import java.util.Collection;
 import java.util.List;
 import java.util.Random;
+import java.util.Scanner;
+
 import org.jdom2.Content;
 import org.jdom2.JDOMException;
 import org.jdom2.Document;
 import org.jdom2.Element;
 import java.util.stream.Collectors;
 import org.jdom2.input.SAXBuilder;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 import javax.annotation.Nonnull;
 
-
 /**
- * Not perfect but works
- * TODO: Needs some rework
+ * Not perfect but works TODO: Needs some rework
  */
 public enum Host {
     /**
      * Gelbooru Host
      */
-    GELBOORU("https://gelbooru.com/", "index.php?page=dapi&s=post&q=index", "", "&pid=",
-            "&tags=", "https://gelbooru.com/index.php?page=post&s=view&id=", "file_url",
-            "peview_url", "source", "tags", "id", Host.ATTRIBUTE),
+    GELBOORU("https://gelbooru.com/", "index.php?page=dapi&s=post&q=index", "", "&pid=", "&tags=",
+            "https://gelbooru.com/index.php?page=post&s=view&id=", "file_url", "peview_url", "source", "tags", "id",
+            Host.ATTRIBUTE),
     /**
      * Safebooru Host
      */
-    SAFEBOORU("https://safebooru.org/", "index.php?page=dapi&s=post&q=index", "", "&pid=",
-            "&tags=", "https://safebooru.org/index.php?page=post&s=view&id=", "file_url",
-            "peview_url", "source", "tags", "id", Host.ATTRIBUTE),
+    SAFEBOORU("https://safebooru.org/", "index.php?page=dapi&s=post&q=index", "", "&pid=", "&tags=",
+            "https://safebooru.org/index.php?page=post&s=view&id=", "file_url", "peview_url", "source", "tags", "id",
+            Host.ATTRIBUTE),
     /**
      * Danbooru Host
      */
     DANBOORU("https://danbooru.donmai.us/", "posts.xml?", "counts/", "page=", "&tags=",
-            "https://danbooru.donmai.us/posts/", "large-file-url",
-            "preview-file-url", "source", "tag-string", "id", Host.CHILD) {
-    };
+            "https://danbooru.donmai.us/posts/", "large-file-url", "preview-file-url", "source", "tag-string", "id",
+            Host.CHILD),
+    NEKOAPI("https://nekos.life/api/v2/img/", null, null, null, null, null, null, null, null, null, null, null);
 
-    private static final  String POST = "[Element: <post/>]";
-    private static final  String CHILD = "child";
+    private static final String POST = "[Element: <post/>]";
+    private static final String CHILD = "child";
     private static final String ATTRIBUTE = "attribute";
     private final String home;
     private final String post;
@@ -59,7 +63,7 @@ public enum Host {
     private final String xmlNode;
 
     Host(String home, String post, String countSubpage, String page, String tags, String postUrl, String sourceUrlQuery,
-         String previewUrlQuery, String sourceQuery, String tagsQuery, String idQuery, String xmlNode) {
+            String previewUrlQuery, String sourceQuery, String tagsQuery, String idQuery, String xmlNode) {
         this.home = home;
         this.post = post;
         this.countSubpage = countSubpage;
@@ -100,43 +104,41 @@ public enum Host {
         Document doc = this.getDocument(tags, page);
         Set<Image> images = new HashSet<>();
         Element rootElement = doc.getRootElement();
-        List<Content> contents = rootElement.getContent().stream().filter(c
-                -> c.toString().equals(POST)).collect(Collectors.toList());
+        List<Content> contents = rootElement.getContent().stream().filter(c -> c.toString().equals(POST))
+                .collect(Collectors.toList());
         int max = contents.size();
         Random rand = new Random();
-            for (int x = 1; x <= amount && x <= max; x++) {
-                int i = rand.nextInt(max);
-                Content c = contents.get(i);
-                String fileUrl;
-                String previewUrl;
-                String source;
-                String[] tagsArray;
-                String id;
-                if (xmlNode.equals(ATTRIBUTE)) {
-                    fileUrl = ((Element) c).getAttributeValue(sourceUrlQuery);
-                    previewUrl = ((Element) c).getAttributeValue(previewUrlQuery);
-                    source = ((Element) c).getAttributeValue(sourceQuery);
-                    tagsArray = ((Element) c).getAttributeValue(tagsQuery).split(" ");
-                    id = ((Element) c).getAttributeValue(idQuery);
-                } else if (xmlNode.equals(CHILD)) {
-                    fileUrl = ((Element) c).getChild(sourceUrlQuery).getValue();
-                    previewUrl = ((Element) c).getChild(sourceUrlQuery).getValue();
-                    source = ((Element) c).getChild(sourceUrlQuery).getValue();
-                    tagsArray = ((Element) c).getChild(sourceUrlQuery).getValue().split(" ", -1);
-                    id = ((Element) c).getChild(sourceUrlQuery).getValue();
-                    images.add(new Image(fileUrl, previewUrl, tagsArray, source, id, this));
-                } else {
-                    throw new IllegalArgumentException("Invalid xml node type");
-                }
+        for (int x = 1; x <= amount && x <= max; x++) {
+            int i = rand.nextInt(max);
+            Content c = contents.get(i);
+            String fileUrl;
+            String previewUrl;
+            String source;
+            String[] tagsArray;
+            String id;
+            if (xmlNode.equals(ATTRIBUTE)) {
+                fileUrl = ((Element) c).getAttributeValue(sourceUrlQuery);
+                previewUrl = ((Element) c).getAttributeValue(previewUrlQuery);
+                source = ((Element) c).getAttributeValue(sourceQuery);
+                tagsArray = ((Element) c).getAttributeValue(tagsQuery).split(" ");
+                id = ((Element) c).getAttributeValue(idQuery);
+            } else if (xmlNode.equals(CHILD)) {
+                fileUrl = ((Element) c).getChild(sourceUrlQuery).getValue();
+                previewUrl = ((Element) c).getChild(sourceUrlQuery).getValue();
+                source = ((Element) c).getChild(sourceUrlQuery).getValue();
+                tagsArray = ((Element) c).getChild(sourceUrlQuery).getValue().split(" ", -1);
+                id = ((Element) c).getChild(sourceUrlQuery).getValue();
                 images.add(new Image(fileUrl, previewUrl, tagsArray, source, id, this));
+            } else {
+                throw new IllegalArgumentException("Invalid xml node type");
+            }
+            images.add(new Image(fileUrl, previewUrl, tagsArray, source, id, this));
         }
         return images;
     }
 
     public Document getDocument(@Nonnull final String tags, final int page) {
-        String url = this.home + this.post
-                + this.page + (page - 1)
-                + this.tags + tags;
+        String url = this.home + this.post + this.page + (page - 1) + this.tags + tags;
         SAXBuilder builder = new SAXBuilder();
         Document document;
         try {
@@ -148,8 +150,7 @@ public enum Host {
     }
 
     public Document getDocument(@Nonnull final String tags) {
-        String url = this.home + this.countSubpage + this.post
-                + this.tags + tags;
+        String url = this.home + this.countSubpage + this.post + this.tags + tags;
         SAXBuilder builder = new SAXBuilder();
         Document document;
         try {
@@ -158,5 +159,43 @@ public enum Host {
             throw new IllegalArgumentException(e.getMessage());
         }
         return document;
+    }
+
+    public static Collection<Image> getNekos(final String tag, final int amount) throws IOException {
+        Set<Image> images = new HashSet<>();
+        for (int i = 1; i <= amount; i++) {
+            URL url = new URL(String.format("https://nekos.life/api/v2/img/%s", tag));
+            InputStream is = url.openStream();
+
+            Scanner sc = new Scanner(is, "UTF-8").useDelimiter("\\A");
+            String dataString = sc.next();
+            sc.close();
+            JSONObject json = null;
+            try {
+                json = (JSONObject) new JSONParser().parse(dataString);
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+            if (json != null){
+                String source = (String) json.get("url");
+                images.add(new Image(source, source, new String[]{tag}, source, source, Host.NEKOAPI));
+            }
+        }
+        return images;
+    }
+
+    //TODO: test main remove when everything works
+    public static void main(String[] args) {
+        Collection<Image> images;
+        try {
+            images = Host.getNekos("neko", 2);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return;
+        }
+
+        for (Image i : images) {
+            System.out.println(i.getUrl());
+        }
     }
 }
